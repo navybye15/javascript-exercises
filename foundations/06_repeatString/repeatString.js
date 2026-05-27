c312daf3244bd0ff736d57f996c874e5ed9c36aa
Ivan Capanay <123456789+navybye15@users.noreply.github.com>
2026-05-27 16:15:38 +0800

    Answered exercise number 4,5,6 and 8 completely

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
 const repeatString = function() {
 
+    let num;
+    let string;
+    let i;
+    if (num <0){
+        string = "ERROR";
+    } else {
+        for ( i = 1; i <= num; i++ ){
+            return string;
+        }
+    }
 };
 
 // Do not edit below this line
