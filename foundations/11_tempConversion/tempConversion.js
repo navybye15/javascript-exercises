82acb42f1daf17e1fbd2206dd0899b11fbcb8c4a
Ivan Capanay <123456789+navybye15@users.noreply.github.com>
2026-06-13 15:55:27 +0800

    answered 10 and 11. Still need to master arrays

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let result = Number(((farenheit-32) * (5/9)).toFixed(1));
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = Number(((celsius*1.8) + (32)).toFixed(1));
+  return result;
 };
 
 // Do not edit below this line
